40b8596fcb1894bf02bcfa0fe58b7d6fa9ec3133
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-07-02 08:25:58 +0900

    Require NIC ID in ASR protected VM data

--- 20-asr/locals.tf
+++ 20-asr/locals.tf
@@ -1,11 +1,11 @@
 locals {
-  asr_settings_raw = csvdecode(file("${path.module}/data/asr_settings.csv"))
+  asr_settings_raw  = csvdecode(file("${path.module}/data/asr_settings.csv"))
   protected_vms_raw = csvdecode(file("${path.module}/data/protected_vms.csv"))
 
   asr_settings = {
     for s in local.asr_settings_raw : s.key => merge(s, {
-      recovery_point_retention_in_minutes                    = tonumber(s.recovery_point_retention_in_minutes)
-      application_consistent_snapshot_frequency_in_minutes   = tonumber(s.application_consistent_snapshot_frequency_in_minutes)
+      recovery_point_retention_in_minutes                  = tonumber(s.recovery_point_retention_in_minutes)
+      application_consistent_snapshot_frequency_in_minutes = tonumber(s.application_consistent_snapshot_frequency_in_minutes)
     })
   }
 
@@ -13,7 +13,7 @@ locals {
 
   protected_vms = {
     for vm in local.protected_vms_raw : vm.replication_name => vm
-    if !startswith(vm.source_vm_id, "<") && !startswith(vm.os_disk_id, "<")
+    if !startswith(vm.source_vm_id, "<") && !startswith(vm.os_disk_id, "<") && !startswith(vm.source_nic_id, "<")
   }
 
   target_resource_group_names = toset([
