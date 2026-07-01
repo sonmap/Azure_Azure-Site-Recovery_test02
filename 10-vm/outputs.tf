aa733e60cba3cdd28f1eab40d5ac55a46085b123
Son kyoungwon <78362051+sonmap@users.noreply.github.com>
2026-07-02 08:25:48 +0900

    Output NIC IDs for ASR replication

--- 10-vm/outputs.tf
+++ 10-vm/outputs.tf
@@ -10,6 +10,12 @@ output "vm_os_disk_ids" {
   }
 }
 
+output "vm_nic_ids" {
+  value = {
+    for name, nic in azurerm_network_interface.nic : name => nic.id
+  }
+}
+
 output "private_ip_addresses" {
   value = {
     for name, nic in azurerm_network_interface.nic : name => nic.private_ip_address
@@ -17,5 +23,5 @@ output "private_ip_addresses" {
 }
 
 output "next_step_20_asr_protected_vms_csv" {
-  value = "Copy vm_ids and vm_os_disk_ids into 20-asr/data/protected_vms.csv before applying 20-asr."
+  value = "Copy vm_ids, vm_os_disk_ids, and vm_nic_ids into 20-asr/data/protected_vms.csv before applying 20-asr."
 }
